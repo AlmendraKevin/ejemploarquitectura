aead07e6547f1c5faf5b71bab54a876320e621e1
AlmendraKevin <kevin.almendra@uabc.edu.mx>
2020-03-09 22:50:24 -0700

    Initial commit

---
+++ src/main/java/com/uabc/edu/arquitectura/ejemploarquitectura/repository/EmpleadoRepository.java
@@ -0,0 +1,13 @@
+package com.uabc.edu.arquitectura.ejemploarquitectura.repository;
+
+
+import com.uabc.edu.arquitectura.ejemploarquitectura.model.Empleado;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmpleadoRepository extends JpaRepository<Empleado,Integer> {
+
+
+
+}
---
+++ src/main/java/com/uabc/edu/arquitectura/ejemploarquitectura/service/EmpleadoService.java
@@ -0,0 +1,32 @@
+package com.uabc.edu.arquitectura.ejemploarquitectura.service;
+
+import com.uabc.edu.arquitectura.ejemploarquitectura.model.Empleado;
+import com.uabc.edu.arquitectura.ejemploarquitectura.repository.EmpleadoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class EmpleadoService {
+
+    @Autowired
+    private EmpleadoRepository repo;
+
+    public List<Empleado> getEmpleados()
+    {
+        return repo.findAll();
+    }
+    public Optional<Empleado> getEmpleadoById(Integer id){
+        return  repo.findById(id);
+    }
+
+    public void saveEmpleado(Empleado emp) {
+       repo.save(emp);
+    }
+
+    public void deleteEmpleado(Integer id) {
+        repo.deleteById(id);
+    }
+}
